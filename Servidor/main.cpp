87f48735639a5605a63e48748fc8edbb8c1d50a3
martin-steglich <77.tincho@gmail.com>
2015-09-14 23:06:28 -0300

    Creación de los proyectos e importación a git

---
+++ Servidor/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Tincho
+ *
+ * Created on September 14, 2015, 11:04 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
