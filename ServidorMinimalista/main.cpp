0dcd96bc534a5d85b7cccc4d4f4a7a053b988bef
Bruno Amaral <bruno.amaral@fing.edu.uy>
2015-09-24 19:56:51 -0300

    Servidor re Vago

---
+++ ServidorMinimalista/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstdlib>
+//#include "Cliente.h"
+//#include "HiloAtiende.h"
+//#include "Errores.h"
+//#include "ParserComandos.h"
+#include <iostream>
+#include <string>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <resolv.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+
+
+
+
+using namespace std;
+
+
+
+//Puerto
+#define PORT 54321
+
+//Número máximo de hijos
+#define MAX_HIJOS 10 // TODO para empezar acordarse de poner un numero mayor
+
+//Longitud del buffer
+#define BUFFERSIZE 512 // poner el de los datagramas
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    char buffer[BUFFERSIZE];
+    fd_set socketsActuales;
+
+    int socketServidorAtiendeLogin;
+    struct sockaddr_in myDireccion;
+    struct sockaddr_in clienteDireccion;
+
+    socketServidorAtiendeLogin = socket(PF_INET,SOCK_DGRAM, 0);
+    if(socketServidorAtiendeLogin == -1)
+      cout << "No puedo inicializar el socket";
+
+    myDireccion.sin_family = PF_INET ; //indica el dominio de comunicacion a utilizar
+    myDireccion.sin_port = htons(PORT); //puerto htons transforma el int a una ip
+    myDireccion.sin_addr.s_addr = htonl(INADDR_ANY); //ip en la que escucha INADDR_ANY significa cualquiera
+
+    int contador = 0;
+
+
+    if( bind( socketServidorAtiendeLogin, (struct sockaddr*)&myDireccion, sizeof(myDireccion)) == -1 )
+      cout << "No Puede Hacer Bind" << endl;
+
+    while ( contador < 2 ){
+
+    socklen_t len = sizeof clienteDireccion;
+
+
+    FD_ZERO(&socketsActuales);
+    FD_SET(socketServidorAtiendeLogin, &socketsActuales);
+
+    recvfrom(socketServidorAtiendeLogin, buffer ,BUFFERSIZE, 0 , (struct sockaddr*)&clienteDireccion, &len);
+    cout << "mensaje recibido"<< endl;
+    cout << inet_ntoa(clienteDireccion.sin_addr) << endl;
+    cout << clienteDireccion.sin_port << endl;
+    cout << buffer << endl;
+
+    clienteDireccion.sin_port = htons(40291);
+    cout << clienteDireccion.sin_port << endl;
+     if(sendto(socketServidorAtiendeLogin, buffer, strlen(buffer)+1, 0 , (struct sockaddr*)&clienteDireccion , sizeof(clienteDireccion) ) == -1)
+        cout << "No Puede mandar msj";
+
+   cout << clienteDireccion.sin_port << endl;
+
+
+       contador++;
+    }
+
+    close(socketServidorAtiendeLogin);
+
+    return 0;
+}
